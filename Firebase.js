a46389312b4ccb6899b2433eff16efc8b5bcaab5
Subhasmita Mishra <msubhasmita29@gmail.com>
2026-06-12 20:29:56 +0530

    firebase.js getting the key

--- Firebase.js
+++ Firebase.js
@@ -1,5 +1,12 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json");
+
+let serviceAccount;
+
+try {
+  serviceAccount = require("./serviceAccountKey.json");
+} catch (error) {
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
