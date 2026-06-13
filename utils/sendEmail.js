01ee24ccb725405aab0d415e70f4f66e65843990
Subhasmita Mishra <msubhasmita29@gmail.com>
2026-06-13 16:50:04 +0530

    otp

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -2,17 +2,21 @@ const nodemailer = require('nodemailer');
 
 async function sendEmail(to, subject, html) {
     const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 587,          
-        secure: false,      
+        host: "smtp.gmail.com",
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
+            pass: process.env.EMAIL_PASS,
+        },
     });
 
+    await transporter.verify();
+    console.log("SMTP connected");
+
     await transporter.sendMail({
-        from: `"BingoBing" <${process.env.EMAIL_USER}>`, 
+        from: `"BingoBing" <${process.env.EMAIL_USER}>`,
+        to,
         subject,
         html
     });
